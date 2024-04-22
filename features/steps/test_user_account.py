bc3525604e7e50d494c74f5f8e928c6c351698e5
izatismail@gmail.com <mybleuwy59>
2024-04-22 21:09:02 +0800

    use case 2: passed

--- features/steps/test_user_account.py
+++ features/steps/test_user_account.py
@@ -31,55 +31,30 @@ def step_impl(context):
 
 @when('I click on the profile button')
 def step_impl(context):
-    # debugging, to see if element is clickable
-    profile_icon = context.browser.find_element(By.XPATH, "//div[@aria-label='nav-user-button']")
-    WebDriverWait(context.browser, 20).until(
-        lambda x: profile_icon.is_displayed() and profile_icon.size['height'] > 0 and profile_icon.size['width'] > 0
+    profile_icon = WebDriverWait(context.browser, 10).until(
+        EC.visibility_of_element_located((By.CSS_SELECTOR, "#nav-desktop ._tid_nav-user-button .flex"))
     )
-    context.browser.execute_script("arguments[0].scrollIntoView(true);", profile_icon)
 
-
-    # Then use ActionChains to perform the hover
-    hover = ActionChains(context.browser).move_to_element(profile_icon)
-    hover.perform()
-    hover.click(profile_icon).perform()
-
-
-    # both attempt using wait then click and force js failed
-    #WebDriverWait(context.browser, 10).until(
-    #    EC.element_to_be_clickable((By.XPATH, "//div[@aria-label='nav-user-button']"))
-    #).click()
-    #context.browser.execute_script("arguments[0].click();", element)
+    profile_icon.click()
 
 
 @when('I click on the "My user account"')
 def step_impl(context):
-    # Wait for the dropdown to be visible and interactable
-    WebDriverWait(context.browser, 10).until(
-        EC.visibility_of_element_located((By.CSS_SELECTOR, "div[aria-label='select-item-user-profile']"))
+    user_account_menu = WebDriverWait(context.browser, 10).until(
+        EC.visibility_of_element_located((By.CSS_SELECTOR, "#user-profile > .truncate"))
     )
 
-    # Click on the dropdown item for "My user account"
-    user_account_option = context.browser.find_element(By.ID, "user-profile")
-    user_account_option.click()
-
-
-@then('I should see "My user account" menu')
-def step_impl(context):
-    # This could be validated by checking the URL or a specific element on the page
-    assert context.browser.current_url == "https://vsmonitor.com/user/profile", \
-        f"Expected URL to be 'https://vsmonitor.com/user/profile' but was {context.browser.current_url}"
-
-    assert context.browser.title == "My user account" in context.browser.title
+    user_account_menu.click()
 
 
 @then('I should see "My user account" page')
 def step_impl(context):
-    # This could be validated by checking the URL or a specific element on the page
-    assert context.browser.current_url == "https://vsmonitor.com/user/profile", \
-        f"Expected URL to be 'https://vsmonitor.com/user/profile' but was {context.browser.current_url}"
+    WebDriverWait(context.browser, 15).until(
+        EC.title_is(util_cfg.PageTitle.USER_ACCOUNT)
+    )
 
-    assert context.browser.title == "My user account" in context.browser.title, "Not on My user account page."
+    assert context.browser.current_url == util_cfg.URLConfig.USER_ACCOUNT, \
+                        f"Expected URL to be {util_cfg.URLConfig.USER_ACCOUNT} but was {context.browser.current_url}"
 
 
 @then('I should see correct name and email address')
@@ -89,10 +64,16 @@ def step_impl(context):
     correct_lname = "Ismai"
     correct_email = "izatismail@gmail.com"
 
-    first_name_element = context.browser.find_element(By.ID, "firstName")
-    last_name_element = context.browser.find_element(By.ID, "lastName")
-    email_element = context.browser.find_element(By.ID, "email")
+    # Locate the input element and get its value
+    first_name_input = context.browser.find_element(By.ID, "firstName")
+    first_name_value = first_name_input.get_attribute('value')
 
-    assert first_name_element.text == correct_fname, f"Expected first name {correct_fname}, but found {first_name_element}"
-    assert last_name_element.text == correct_lname, f"Expected last name {correct_lname}, but found {last_name_element}"
-    assert email_element.text == correct_email, f"Expected email {correct_email}, but found {email_element.text}"
+    last_name_input = context.browser.find_element(By.ID, "lastName")
+    last_name_value = last_name_input.get_attribute('value')
+
+    email_input = context.browser.find_element(By.ID, "email")
+    email_value = email_input.get_attribute('value')
+
+    assert first_name_value == correct_fname, f"Expected first name {correct_fname}, but found {first_name_element}"
+    assert last_name_value == correct_lname, f"Expected last name {correct_lname}, but found {last_name_element}"
+    assert email_value == correct_email, f"Expected email {correct_email}, but found {email_element.text}"
